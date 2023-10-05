c84c7d703b725b85ab7d6a10ec4f7561e234d27c
Nandana Santhosh <nandanapsmail@gmail.com>
2023-10-05 21:09:47 +0530

    new

---
+++ newapp.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+import cv2
+
+# Load the model
+model = load_model("keras_Model.h5")
+
+# Load the labels
+class_names = open("labels.txt", "r").readlines()
+
+# Create a Streamlit web app
+st.title("Webcam Image Classification")
+
+# Function to predict and display the results
+def predict(image):
+    image = cv2.resize(image, (224, 224))
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+    image = (image / 127.5) - 1
+
+    prediction = model.predict(image)
+    index = np.argmax(prediction)
+    class_name = class_names[index]
+    confidence_score = prediction[0][index]
+
+    return class_name, confidence_score
+
+# Access the webcam
+cap = cv2.VideoCapture(0)
+
+# Main Streamlit loop
+while True:
+    # Read a frame from the webcam
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    # Perform prediction
+    class_name, confidence_score = predict(frame)
+
+    # Display the webcam feed and prediction results
+    st.image(frame, channels="BGR", use_column_width=True, caption="Webcam Feed")
+    st.write(f"Class: {class_name[2:]}")
+    st.write(f"Confidence Score: {str(np.round(confidence_score * 100))[:-2]} %")
+
+    # Check for Streamlit app exit
+    if st.button("Exit"):
+        break
+
+# Release the webcam
+cap.release()
