6960baca39b78448865a8ff90a5c17b9c558250d
Nandana Santhosh <nandanapsmail@gmail.com>
2023-10-05 17:43:05 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,72 +1,53 @@
 import streamlit as st
-import tensorflow as tf
-import numpy as np
-import time
-import os
-os.environ["DISPLAY"] = ":0"  # Set the DISPLAY environment variable
+import io
 import cv2
+import numpy as np
+import tensorflow as tf
 
+# Function to load the Keras model and labels
+def load_model_and_labels(model_path, labels_path):
+    model = tf.keras.models.load_model(model_path)
+    with open(labels_path, "r") as file:
+        class_names = [line.strip() for line in file.readlines()]
+    return model, class_names
 
-# Load your TensorFlow model and labels here
-model = tf.keras.models.load_model("keras_model.h5")
-class_names = open("labels.txt", "r").readlines()
-
-# Initialize webcam
-camera = cv2.VideoCapture(0)
-
-# Function to perform predictions
-def predict(image):
-    # Preprocess the image as needed
-
-    # Resize the image to match the model's input shape
+# Function to preprocess the image
+def preprocess_image(image):
     image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
-
-    # Make the image a numpy array and reshape it to the model's input shape
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
-
-    # Normalize the image array
     image = (image / 127.5) - 1
+    return image
 
-    # Predict using the loaded model
-    prediction = model.predict(image)
-    index = np.argmax(prediction)
-    class_name = class_names[index]
-    confidence_score = prediction[0][index]
+# Streamlit app
+def main():
+    st.title("Image Classification App")
+    st.sidebar.header("Settings")
 
-    return class_name, confidence_score
+    # Upload an image
+    uploaded_image = st.sidebar.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
 
-# Streamlit app title
-st.title("Lettucespinach model")
+    if uploaded_image:
+        # Load the model and labels
+        model, class_names = load_model_and_labels("keras_Model.h5", "labels.txt")
 
-# Add a Start button to initiate webcam feed
-start_button = st.button("Start")
+        # Read the uploaded image using OpenCV
+        image = cv2.imdecode(np.fromstring(uploaded_image.read(), np.uint8), 1)
 
-if start_button:
-    num_inputs = 5  # Define the number of inputs to process
-    input_counter = 0  # Initialize the input counter
+        # Display the uploaded image
+        st.image(image, caption="Uploaded Image", use_column_width=True)
 
-    while input_counter < num_inputs:
-        ret, frame = camera.read()
-        if not ret:
-            st.text("Error: Unable to capture webcam feed.")
-            break
+        # Preprocess the image and make a prediction
+        processed_image = preprocess_image(image)
+        prediction = model.predict(processed_image)
+        index = np.argmax(prediction)
+        class_name = class_names[index]
+        confidence_score = prediction[0][index]
 
-        # Perform prediction on the frame
-        class_name, confidence_score = predict(frame)
+        # Display the prediction and confidence score
+        st.subheader("Prediction:")
+        st.write(f"Class: {class_name}")
+        st.write(f"Confidence Score: {np.round(confidence_score * 100, 2)}%")
 
-        # Display the webcam feed
-        st.image(frame, caption="Webcam Image", use_column_width=True)
-
-        # Display prediction results
-        st.subheader("Prediction Results:")
-        st.text(f"Class: {class_name[2:]}")
-        st.text(f"Confidence Score: {np.round(confidence_score * 100, 2)}%")
-
-        input_counter += 1  # Increment the input counter
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-# Release the webcam and close OpenCV window
-camera.release()
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    main()
